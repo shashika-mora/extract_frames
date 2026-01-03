fa94cc2fe7427bc5254d72534023b526f8b50504
A.M.S.T Dayarathna <shashikatheekshana67@gmail.com>
2026-01-03 23:13:12 +0530

    feat: Use `test.mp4` for video processing, log extracted frame count, and add `test.mp4` as an example.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import cv2
 
-video = cv2.VideoCapture("video.mp4")
+video = cv2.VideoCapture("test.mp4")
+# Replace with your video or with a file path
 
 count, success = 0, True
 while success:
     success, frame = video.read() # Read Frame
     if success:
-        cv2.imwrite(f"frame_{count}.jpg", frame)
+        cv2.imwrite(f"frame_{count}.jpg", frame) # Save Frame
         count += 1
-    
-video.release()
+    else:
+        break
+
+video.release() # Release Video
+print(f"Extracted {count} frames.")
 cv2.destroyAllWindows()
